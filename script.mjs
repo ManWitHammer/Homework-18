54be38de0a27c7f090a65c51bee5777c116a70d5
ManWitHammer <nikitaz.post@mail.ru>
2024-02-08 23:27:34 +0300

    filter and sort

--- script.mjs
+++ script.mjs
@@ -7,9 +7,156 @@ const URL = 'http://localhost:3000/USERS'
 const form = document.querySelector('form')
 const buttonPost = document.querySelector('#buttonPost')
 const list = document.querySelector('.list')
+const ascending = document.querySelector('.ascending')
+const descending = document.querySelector('.descending')
+const completed = document.querySelector('.completed')
+const uncompleted = document.querySelector('.uncompleted')
+const all = document.querySelector('.all')
+
+ascending.addEventListener('click', async e => {
+	e.preventDefault()
+	try {
+		let data1 = []
+		const data = await getData(URL)
+		const uName = document.querySelectorAll('.list .user-profile')
+		uName.forEach(user => {
+			data.forEach(user1 => {
+				if (user1.id == user.id) {
+					data1.push(user1)
+				}
+			})
+		})
+		list.innerHTML = ''
+		data1.forEach(user => {
+			list.insertAdjacentHTML(
+				`afterbegin`,
+				`
+				<li class="user-profile ${user.priority}" id="${user.id}">
+					<div class="material-symbols-outlined deleteBlock"> delete </div>
+					${user.done == true ? '<div class="material-symbols-outlined completeBlock"> done </div>' : '<div class="material-symbols-outlined completeBlock"> close </div>'}
+					${user.done == true ? `<p class="title">${user.title}</p>` : `<p class="title"><del>${user.title}</del></p>`}
+					${user.done == true ? `<p class="description">${user.description}</p>` : `<p class="description"><del>${user.description}</del></p>`}
+				</li>
+				`
+			)
+		})
+		//удаление задач
+	} catch (error) {
+		console.error(error)
+	}
+})
+
+descending.addEventListener('click', async e => {
+	e.preventDefault()
+	try {
+		let data1 = []
+		const data = await getData(URL)
+		const uName = document.querySelectorAll('.list .user-profile')
+		uName.forEach(user => {
+			data.forEach(user1 => {
+				if (user1.id == user.id) {
+					data1.push(user1)
+				}
+			})
+		})
+		list.innerHTML = ''
+		data1.forEach(user => {
+			list.insertAdjacentHTML(
+				`afterbegin`,
+				`
+				<li class="user-profile ${user.priority}" id="${user.id}">
+					<div class="material-symbols-outlined deleteBlock"> delete </div>
+					${user.done == true ? '<div class="material-symbols-outlined completeBlock"> done </div>' : '<div class="material-symbols-outlined completeBlock"> close </div>'}
+					${user.done == true ? `<p class="title">${user.title}</p>` : `<p class="title"><del>${user.title}</del></p>`}
+					${user.done == true ? `<p class="description">${user.description}</p>` : `<p class="description"><del>${user.description}</del></p>`}
+				</li>
+				`
+			)
+		})
+		//удаление задач
+	} catch (error) {
+		console.error(error)
+	}
+})
+
+completed.addEventListener('click', async e => {
+	e.preventDefault()
+	
+	try {
+		const data = await getData(URL)
+		list.innerHTML = ''
+		data.forEach(user => {
+			if (user.done == true) {
+				list.insertAdjacentHTML(
+					`beforeend`,
+					`
+					<li class="user-profile ${user.priority}" id="${user.id}">
+						<div class="material-symbols-outlined deleteBlock"> delete </div>
+						${user.done == true ? '<div class="material-symbols-outlined completeBlock"> done </div>' : '<div class="material-symbols-outlined completeBlock"> close </div>'}
+						${user.done == true ? `<p class="title">${user.title}</p>` : `<p class="title"><del>${user.title}</del></p>`}
+						${user.done == true ? `<p class="description">${user.description}</p>` : `<p class="description"><del>${user.description}</del></p>`}
+					</li>
+					`
+				)
+			}
+		})
+		//удаление задач
+	} catch (error) {
+		console.error(error)
+	}
+})
+
+uncompleted.addEventListener('click', async e => {
+	e.preventDefault()
+	try {
+		const data = await getData(URL)
+		list.innerHTML = ''
+		data.forEach(user => {
+			if (user.done == false) {
+				list.insertAdjacentHTML(
+					`beforeend`,
+					`
+					<li class="user-profile ${user.priority}" id="${user.id}">
+						<div class="material-symbols-outlined deleteBlock"> delete </div>
+						${user.done == true ? '<div class="material-symbols-outlined completeBlock"> done </div>' : '<div class="material-symbols-outlined completeBlock"> close </div>'}
+						${user.done == true ? `<p class="title">${user.title}</p>` : `<p class="title"><del>${user.title}</del></p>`}
+						${user.done == true ? `<p class="description">${user.description}</p>` : `<p class="description"><del>${user.description}</del></p>`}
+					</li>
+					`
+				)
+			}
+		})
+		//удаление задач
+	} catch (error) {
+		console.error(error)
+	}
+})
+
+all.addEventListener('click', async e => {
+	e.preventDefault()
+	try {
+		const data = await getData(URL)
+		list.innerHTML = ''
+		data.forEach(user => {
+			list.insertAdjacentHTML(
+				`afterbegin`,
+				`
+				<li class="user-profile ${user.priority}" id="${user.id}">
+					<div class="material-symbols-outlined deleteBlock"> delete </div>
+					${user.done == true ? '<div class="material-symbols-outlined completeBlock"> done </div>' : '<div class="material-symbols-outlined completeBlock"> close </div>'}
+					${user.done == true ? `<p class="title">${user.title}</p>` : `<p class="title"><del>${user.title}</del></p>`}
+					${user.done == true ? `<p class="description">${user.description}</p>` : `<p class="description"><del>${user.description}</del></p>`}
+				</li>
+				`
+			)
+		})
+	} catch (error) {
+		console.error(error)
+	}
+})
 
 //добавление задач
-buttonPost.addEventListener('click', e => {
+buttonPost.addEventListener('click', async e => {
 	e.preventDefault()
 	try {
 		const data = new FormData(form)
@@ -18,9 +165,9 @@ buttonPost.addEventListener('click', e => {
 			title: data.get('title'),
 			description: data.get('details'),
 			priority: data.get('priority'),
-			done: false
+			done: true
 		}
-		postData(URL, user)
+		await postData(URL, user)
 		form.reset()
 	} catch (error) {
 		console.error(error)
@@ -62,7 +209,7 @@ document.addEventListener('DOMContentLoaded', async (e) => {
 				e.preventDefault()
 				const id = delButton.parentElement.id
 				try {
-					deleteData(`http://localhost:3000/USERS/${id}`).then(response => {
+					await deleteData(`http://localhost:3000/USERS/${id}`).then(response => {
 						console.log(response, `пользователь с id = ${id} успешно удалён`)
 					})
 				} catch (err) {
